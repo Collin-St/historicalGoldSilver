e5e3016361fa41330eeb20accbe8c90116888ca9
collin-st <collin.street.dr@gmail.com>
2019-05-08 00:54:06 -0700

    Updated spider to log Data and Price columns

--- historicalGoldSilver/historicalGoldSilver/spiders/prices_spider.py
+++ historicalGoldSilver/historicalGoldSilver/spiders/prices_spider.py
@@ -14,8 +14,17 @@ class PricesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split('/')[-2]
-        filename = 'prices-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for row in response.xpath("//table[@id='curr_table']//tbody//tr"):
+            yield {
+                'date': row.xpath('td[1]//text()').extract_first(),
+                'price': row.xpath('td[2]//text()').extract_first()
+            }
+
+            # page = response.url.split('/')[-2]
+            # filename = 'prices-%s.html' % page
+            # with open(filename, 'wb') as f:
+            #     f.write(response.body)
+            # self.log('Saved file %s' % filename)
+
+# date column xpath = //table[@id='curr_table']//tbody//tr//td[1]
+# price column xpath = //table[@id='curr_table']//tbody//tr//td[2]
