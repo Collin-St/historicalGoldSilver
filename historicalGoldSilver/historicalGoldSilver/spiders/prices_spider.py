a3671f1d622fbf31a43ff7ed9c20af41d77143b3
collin-st <collin.street.dr@gmail.com>
2019-05-08 04:10:33 -0700

    Stored data in "items" in order to insert into db

--- historicalGoldSilver/historicalGoldSilver/spiders/prices_spider.py
+++ historicalGoldSilver/historicalGoldSilver/spiders/prices_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import HistoricalgoldsilverItem
 
 
 class PricesSpider(scrapy.Spider):
@@ -14,17 +15,26 @@ class PricesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for row in response.xpath("//table[@id='curr_table']//tbody//tr"):
-            yield {
-                'date': row.xpath('td[1]//text()').extract_first(),
-                'price': row.xpath('td[2]//text()').extract_first()
-            }
 
-            # page = response.url.split('/')[-2]
-            # filename = 'prices-%s.html' % page
-            # with open(filename, 'wb') as f:
-            #     f.write(response.body)
-            # self.log('Saved file %s' % filename)
+        items = HistoricalgoldsilverItem()
+
+        for row in response.xpath("//table[@id='curr_table']//tbody//tr"):
+
+            metal = response.xpath(
+                "//h2[@class='float_lang_base_1 inlineblock']//text()").extract_first()
+            date = row.xpath('td[1]//text()').extract_first()
+            price = row.xpath('td[2]//text()').extract_first()
+            mean = '1'
+            variance = '1'
+
+            items['metal'] = metal
+            items['date'] = date
+            items['price'] = price
+            items['mean'] = mean
+            items['variance'] = variance
+
+            yield items
+
 
 # date column xpath = //table[@id='curr_table']//tbody//tr//td[1]
 # price column xpath = //table[@id='curr_table']//tbody//tr//td[2]
